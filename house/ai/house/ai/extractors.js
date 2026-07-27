8e4ea3ebba29786534aa2f3ec1dd48eb7e772446
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-07-27 21:01:18 +0330

    Update extractors.js

--- house/ai/house/ai/extractors.js
+++ house/ai/house/ai/extractors.js
@@ -1,1 +1,39 @@
+const Extractors = {
 
+    version: "1.0",
+
+    async extract(article){
+
+        return {
+
+            title: article.title,
+
+            summary: "",
+
+            capabilities: {
+
+                capability1: "",
+
+                capability2: "",
+
+                capability3: "",
+
+                capability4: "",
+
+                capability5: "",
+
+                capability6: "",
+
+                capability7: ""
+
+            },
+
+            keywords: [],
+
+            status: "waiting_ai"
+
+        };
+
+    }
+
+};
