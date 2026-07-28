cba8eb6f39869397bc5b03b1b2b460146c170a16
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-07-28 12:47:29 +0330

    Update storage.js

--- house/ai/storage.js
+++ house/ai/storage.js
@@ -1,1 +1,39 @@
+const StorageManager = {
 
+    key: "RTAI_MEMORY",
+
+    async load(defaultMemory) {
+
+        const local = localStorage.getItem(this.key);
+
+        if (local) {
+
+            return JSON.parse(local);
+
+        }
+
+        localStorage.setItem(
+            this.key,
+            JSON.stringify(defaultMemory)
+        );
+
+        return defaultMemory;
+
+    },
+
+    save(memory) {
+
+        localStorage.setItem(
+            this.key,
+            JSON.stringify(memory)
+        );
+
+    },
+
+    clear() {
+
+        localStorage.removeItem(this.key);
+
+    }
+
+};
