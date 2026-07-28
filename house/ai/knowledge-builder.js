45529c8f9ddff6bd980a8a63e286f0b8e97c2622
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-07-28 12:58:40 +0330

    Update knowledge-builder.js

--- house/ai/knowledge-builder.js
+++ house/ai/knowledge-builder.js
@@ -1,1 +1,33 @@
+const KnowledgeBuilder = {
 
+    version: "1.0",
+
+    async build(article){
+
+        return {
+
+            id: article.title,
+
+            title: article.title,
+
+            summary: "",
+
+            sevenCapabilities: [
+
+                "", "", "", "", "", "", ""
+
+            ],
+
+            keywords: [],
+
+            relations: [],
+
+            created: new Date().toISOString(),
+
+            status: "waiting"
+
+        };
+
+    }
+
+};
