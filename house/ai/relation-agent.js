e32a0a3c49b976e0dfab46dbc6ad05f223561807
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-07-28 15:39:02 +0330

    Update relation-agent.js

--- house/ai/relation-agent.js
+++ house/ai/relation-agent.js
@@ -1,1 +1,56 @@
+const RelationAgent = {
 
+    name: "Relation Agent",
+
+    version: "1.0",
+
+    async scan(memory){
+
+        const ids = Object.keys(memory.articles);
+
+        let created = 0;
+
+        for(let i=0;i<ids.length;i++){
+
+            for(let j=i+1;j<ids.length;j++){
+
+                const a = memory.articles[ids[i]];
+                const b = memory.articles[ids[j]];
+
+                const common = [];
+
+                for(const tag of a.keywords.fa){
+
+                    if(b.keywords.fa.includes(tag)){
+
+                        common.push(tag);
+
+                    }
+
+                }
+
+                if(common.length>0){
+
+                    a.relations.articles.push(b.id);
+
+                    b.relations.articles.push(a.id);
+
+                    created++;
+
+                }
+
+            }
+
+        }
+
+        return {
+
+            scanned: ids.length,
+
+            processed: created
+
+        };
+
+    }
+
+};
